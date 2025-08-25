da47b932671bde3148a332673f2506c7a45a7251
Prateek345 <prateeksingh.ps345@gmail.com>
2025-08-25 20:32:46 +0530

    init

--- app/src/main/java/com/example/flowsbasics/MainActivity.kt
+++ app/src/main/java/com/example/flowsbasics/MainActivity.kt
@@ -1,47 +1,43 @@
 package com.example.flowsbasics
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.flowsbasics.ui.theme.FlowsBasicsTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    val channel = Channel<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        producer(channel)
+        consumer(channel)
         setContent {
             FlowsBasicsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun producer(channel: Channel<Int>) {
+    CoroutineScope(Dispatchers.Main).launch {
+        channel.send(1)
+        channel.send(2)
+        channel.send(3)
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FlowsBasicsTheme {
-        Greeting("Android")
+fun consumer(channel: Channel<Int>) {
+    CoroutineScope(Dispatchers.Main).launch {
+        Log.d("TAG", channel.receive().toString())
+        Log.d("TAG", channel.receive().toString())
+        Log.d("TAG", channel.receive().toString())
     }
 }
